2ba368dea60b0f1f2a183ac0fb10b66177f039d9
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2021-04-15 10:17:48 +0400

    Part-5 Dependency Injection with Hilt - MVVM Image Search App with Architecture Components & Retrofit #5

--- app/src/main/java/com/sargis/khlopuzyan/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/sargis/khlopuzyan/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -2,11 +2,13 @@ package com.sargis.khlopuzyan.imagesearchapp.ui.gallery
 
 import androidx.fragment.app.Fragment
 import com.sargis.khlopuzyan.imagesearchapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Created by Sargis Khlopuzyan, on 4/8/2021.
  *
  * @author Sargis Khlopuzyan (sargis.khlopuzyan@fastshift.com)
  */
+@AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_gallery) {
 }
